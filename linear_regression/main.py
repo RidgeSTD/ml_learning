182d6b96b04b80021d9aaa4da13b58bf0de893c3
华文澜 <winlandiano@gmail.com>
2017-02-27 18:46:25 +0100

    linear regression finished

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -1,4 +1,8 @@
 import numpy as np
+from scipy import optimize
+
+
+iter_default = 1500
 
 
 def get_data(datafilePath):
@@ -17,28 +21,57 @@ def get_data(datafilePath):
     y = np.zeros(shape=(m, 1))
     for i in range(0, m):
         line = lines[i].split(',')
-        y[i] = line[n]
+        y[i] = float(line[n-1])
         for j in range(1, n):
             x[i, j] = float(line[j-1])
 
+    # normalization
     mu = np.mean(x, 0)
     mu[0] = 0
     scale = np.max(x, 0) - np.min(x, 0)
     scale[0] = 1
     for i in range(0, m):
         x[i, :] = (x[i, :] - mu) / scale
-    test_size = int(m/5)
+    # end of normalization
+
+    #  extracting testing data
+    test_size = int(m / 5)
     x_test = x[0:test_size, :]
     y_test = y[0:test_size, :]
     x = x[test_size:, :]
     y = y[test_size:, :]
     m -= test_size
+
     return m, n, x, y, x_test, y_test
 
 
+def cost_func(theta, x, y, m):
+    # a bug accured solved: theata is a ndarray after numpy.chararray.flatten, whose shape/demension information is lost
+    # therefore we need to reconstruct theta
+    tmp = np.dot(x, theta.reshape(x.shape[1], 1)) - y
+    return np.dot(tmp.transpose(), tmp) / m / 2
+
+
+# def cost_func_prime(theta):
+#     global x
+#     global y
+#     global m
+#     tmp = theta * x.transpose() - y
+
+
 def main_loop(input_filepath):
+    global iter_default
+    print('parsing training and testing data...')
     m, n, x, y, x_test, y_test = get_data(input_filepath)
-    theta = np.random.random_sample()
+    print("finish parsing data, learning...")
+    theta_opt = optimize.fmin_bfgs(cost_func, np.zeros((n, 1)), args=(x, y, m))
+    print('finish learning, get optimized theta with:')
+    print(theta_opt)
+    print('\nprinting estimation(s) on %d testing set' % len(y_test))
+    for i in range(0, len(x_test)):
+        y_est = np.dot(x_test[i, :], theta_opt.transpose())
+        print("%d:\ty_est=%f\t\ty_real=%f" % (i, y_est, y_test[i]))
+
 
 
 if __name__ == '__main__':
@@ -48,6 +81,6 @@ if __name__ == '__main__':
     data_set_num = len(params_lines)
     counter = 0
     for line in params_lines:
-        main_loop(line)
+        main_loop(line.strip())
         counter += 1
-        print("%d of %d dataset(s) finished!", counter, data_set_num)
+        print("%d of %d dataset(s) finished!" % (counter, data_set_num))
